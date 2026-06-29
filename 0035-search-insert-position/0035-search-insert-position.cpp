5cd87120e5a4adb2d087e8651c0d51c34a6a99ef
Ankush Singh Rawat <140985139+ankush850@users.noreply.github.com>
2026-06-29 23:16:28 +0530

    Time: 0 ms (100%), Space: 13.7 MB (42.68%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        int i = 0;
+
+        for (i = 0; i < n; i++) {
+            if (nums[i] >= target) {
+                return i;
+            }
+        }
+        
+        return n;
+    }
+};
