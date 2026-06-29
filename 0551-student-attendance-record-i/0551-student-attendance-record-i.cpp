ce25a22bb15b7860294dab6f2927b8718c8088e9
Ankush Singh Rawat <140985139+ankush850@users.noreply.github.com>
2026-06-29 22:42:31 +0530

    Time: 0 ms (100%), Space: 8.4 MB (51.45%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
@@ -2,13 +2,21 @@ class Solution {
 public:
     bool checkRecord(string s) {
         int absents = 0, lates = 0;
+        
         for (char c : s) {
             absents += (c == 'A');
-            lates = (c == 'L') ? lates + 1 : 0;
             
-            if (absents >= 2 || lates >= 3) 
-            return false;
+            if (c == 'L') {
+                lates = lates + 1;
+            } else {
+                lates = 0;
+            }
+            
+            if (absents >= 2 || lates >= 3) {
+                return false;
+            }
         }
+        
         return true;
     }
 };
