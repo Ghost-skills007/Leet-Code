f1e6f77e33f3b2d362e7f5ab774012a55c68b423
Ankush Singh Rawat <140985139+ankush850@users.noreply.github.com>
2026-07-06 22:29:10 +0530

    Time: 0 ms (100%), Space: 9.7 MB (51.86%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -4,8 +4,8 @@ public:
         int pairs = 0;
         int n = nums.size();
         
-        for (int i = 0; i < n; ++i) {
-        for (int j = i + 1; j < n; ++j) {
+        for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
                 
             if (nums[i] == nums[j]) {
                     pairs++;
