718e1cfe8a16e3dc2bc16cb392edc8c599db1d3b
Ankush Singh Rawat <140985139+ankush850@users.noreply.github.com>
2026-07-17 12:03:13 +0530

    Time: 3 ms (31.39%), Space: 13.4 MB (64.16%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,16 +1,22 @@
 class Solution {
- public:
-  int singleNumber(vector<int>& nums) {
-    int ans = 0;
-
-    for (int i = 0; i < 32; ++i) {
-      int sum = 0;
-      for (const int num : nums)
-        sum += num >> i & 1;
-      sum %= 3;
-      ans |= sum << i;
+public:
+    int singleNumber(std::vector<int>& nums) {
+        int result = 0;
+        
+        for (int i = 0; i < 32; ++i) {
+            int bit_sum = 0;
+            
+            for (int num : nums) {
+                if ((num >> i) & 1) {
+                    bit_sum++;
+                }
+            }
+            
+            if (bit_sum % 3 != 0) {
+                result |= (1 << i);
+            }
+        }
+        
+        return result;
     }
-
-    return ans;
-  }
 };
