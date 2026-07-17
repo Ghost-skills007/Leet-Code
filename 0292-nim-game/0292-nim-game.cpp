1f223235d8bbd783371c25fe842a9cd6af398f78
Ankush Singh Rawat <140985139+ankush850@users.noreply.github.com>
2026-07-17 09:38:29 +0530

    Time: 0 ms (100%), Space: 7.8 MB (16.95%) - LeetHub

---
+++ 0292-nim-game/0292-nim-game.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canWinNim(int n) {
+        if (n <= 3){
+            return true ;
+        }
+        else if( n % 4 != 0 ){
+            return true;
+        }
+        
+        else {
+            return false;
+        }
+    }
+};
