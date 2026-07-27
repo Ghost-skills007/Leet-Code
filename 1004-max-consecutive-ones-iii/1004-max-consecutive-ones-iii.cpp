b42aeb6bc384d39efe7c3e0ef2721c8270d7b1a3
Ankushsrawat <ankushrawat0154@gmail.com>
2026-07-27 11:57:42 +0530

    Time: 4 ms (21.37%), Space: 73.6 MB (31.93%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        int n = nums.size();
-    vector<int> zero_indices;
+      vector<int> z = {-1};
+    for (int i = 0; i < nums.size(); i++) 
+        if (!nums[i]) z.push_back(i);
+    z.push_back(nums.size());
 
-    for (int i = 0; i < n; i++) {
-        if (nums[i] == 0) {
-            zero_indices.push_back(i);
-        }
-    }
+    if (z.size() - 2 <= k) return nums.size();
 
-    int total_zeroes = zero_indices.size();
+    int ans = 0;
+    for (int i = 1; i + k < z.size(); i++) 
+        ans = max(ans, z[i + k] - z[i - 1] - 1);
 
-    if (total_zeroes <= k) {
-        return n;
-    }
-
-    int max_len = 0;
-
-    for (int i = 0; i <= total_zeroes - k; i++) {
-        int left_boundary = (i == 0) ? 0 : zero_indices[i - 1] + 1;
-        int right_boundary = (i + k == total_zeroes) ? n - 1 : zero_indices[i + k] - 1;
-
-        int current_length = right_boundary - left_boundary + 1;
-        max_len = max(max_len, current_length);
-    }
-
-    return max_len;
+    return ans;
     }
 };
