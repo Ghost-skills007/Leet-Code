fb2749a53f5fb5de937dd386dc875ba79fca8730
Ankushsrawat <ankushrawat0154@gmail.com>
2026-07-31 09:02:10 +0530

    Time: 4 ms (56.01%), Space: 21.9 MB (92.98%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+            if (s[i] == s[j]) {
+                i++;
+                j--;
+            } else {
+                int i1 = i + 1, j1 = j;
+                while (i1 < j1 && s[i1] == s[j1]) {
+                    i1++;
+                    j1--;
+                }
+                if (i1 >= j1) return true; 
+                int i2 = i, j2 = j - 1;
+                while (i2 < j2 && s[i2] == s[j2]) {
+                    i2++;
+                    j2--;
+                }
+                if (i2 >= j2) return true; 
+                return false; 
+            }
+        }
+        return true; 
+    }
+};
