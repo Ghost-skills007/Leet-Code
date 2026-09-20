945ae754d3a88424dfb5cf8597e868e6e4b9a95a
Ghost <ankushrawat0154@gmail.com>
2026-09-20 12:02:29 +0530

    Time: 0 ms (100%), Space: 34.5 MB (15.11%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(!root) return nullptr;
+        if(root->val > key) root->left = deleteNode(root->left, key);
+        else if(root->val < key) root->right = deleteNode(root->right, key);
+        else {
+            if(!root->right) return root->left;
+            if(!root->left) return root->right;
+            TreeNode* tmp = root->right;
+            while(tmp->left) tmp = tmp->left;
+            root->val = tmp->val;
+            root->right = deleteNode(root->right, tmp->val);
+        }
+        return root;
+    }
+};
