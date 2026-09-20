7b580273805725acf57442f1bfc0b6b95c46cb23
Ghost <ankushrawat0154@gmail.com>
2026-09-20 17:38:38 +0530

    Time: 0 ms (100%), Space: 19.3 MB (81.19%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,41 +1,25 @@
 class Solution {
 public:
-    vector<int> parent;
-    vector<bool> visited;
-    int ans=0;
-    
-    int find(int x){
-        return parent[x]==x? x:find(parent[x]);
+    void dfs(int node, vector<bool>& visited, vector<vector<int>>& isConnected) {
+        visited[node] = true;
+        int n = isConnected.size();
+        for (int i = 0; i < n; i++) {
+            if (isConnected[node][i] == 1 && !visited[i])
+                dfs(i, visited, isConnected);
+        }
     }
     
-    int findCircleNum(vector<vector<int>>& adjMat) {
-        int n=adjMat.size();
-        parent=vector<int>(n);
-        for(int i=0; i<n; i++){
-            parent[i]=i;
-        }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<bool> visited(n, false);
+        int provinces = 0;
         
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                if(adjMat[i][j]==1){
-                    int a=find(i);
-                    int b=find(j);
-                    
-                    parent[a]=b;
-                }
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i, visited, isConnected);
+                provinces++;
             }
         }
-        visited=vector<bool>(n, false);
-        int i=0;
-        while(i<n){
-            int a=find(i);
-            if(!visited[a]){ 
-                visited[a]=true;
-                ans++;
-            }
-            i++;
-        }
-        
-        return ans;
+        return provinces;
     }
 };
